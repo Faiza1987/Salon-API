307c29a79d1213c97204e92d4813fdff091f0b47
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-07-10 16:30:08 -0700

    Added DRF and JWT to salon-api

---
+++ api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+class User(AbstractUser):
+    username = models.CharField(blank=True, null=True, max_length=10)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return "{}".format(self.email)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                related_name='profile')
+    salon_name = models.CharField(max_length=20)
+    salon_address = models.CharField(max_length=255)
+    salon_city = models.CharField(max_length=255)
+    salon_state = models.CharField(max_length=2)
+    salon_zip = models.CharField(max_length=5)
+    salon_phone_number = models.CharField(max_length=10)
+    salon_description = models.TextField(max_length=2000)
---
+++ api/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from api.models import User, UserProfile
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('salon_name', 'salon_address', 'salon_city',
+                  'salon_state', 'salon_zip', 'salon_phone_number',
+                  'salon_description')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    profile = UserProfileSerializer(required=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'email', 'first_name', 'last_name',
+                  'password', 'profile')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        UserProfile.objects.create(user=user, **profile_data)
+        return user
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile')
+        profile = instance.profile
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        profile.salon_name = profile_data.get(
+            'salon_name', profile.salon_name)
+        profile.salon_address = profile_data.get(
+            'salon_address', profile.salon_address)
+        profile.salon_city = profile_data.get(
+            'salon_city', profile.salon_city)
+        profile.salon_state = profile_data.get(
+            'salon_state', profile.salon_state)
+        profile.salon_zip = profile_data.get(
+            'salon_zip', profile.salon_zip)
+        profile.salon_phone_number = profile_data.get(
+            'salon_phone_number', profile.salon_phone_number)
+        profile.salon_description = profile_data.get(
+            'salon_description', profile.salon_description)
+        profile.save()
+        return instance
+
+
+
+
+
