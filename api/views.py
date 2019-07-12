dba53ad4bf63a1274f2f57dd79e0cb2b22bbf3be
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-07-11 19:04:46 -0700

    Added static files

--- api/views.py
+++ api/views.py
@@ -3,21 +3,25 @@ from api.models import User
 from api.serializers import UserSerializer
 from rest_framework.permissions import AllowAny
 from api.permissions import IsLoggedInUserOrAdmin, IsAdminUser
+from api.models import User, UserProfile
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
     def get_permissions(self):
         permission_classes = []
-        if self.action == 'create' or self.action == 'list':
+        if self.action == 'list' or self.action == 'create':
             permission_classes = [AllowAny]
-
-        elif self.action == 'retreive' or self.action == 'update' \
+        elif self.action == 'retrieve' or self.action == 'update' \
                 or self.action == 'partial_update':
             permission_classes = [IsLoggedInUserOrAdmin]
-
         elif self.action == 'destroy':
             permission_classes = [IsAdminUser]
 
