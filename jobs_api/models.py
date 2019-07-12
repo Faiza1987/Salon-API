13e2ee9ce5a668d89af9dd130de92f0b0de727f4
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-07-12 15:22:30 -0700

    fixed syntax errors

--- jobs_api/models.py
+++ jobs_api/models.py
@@ -8,7 +8,7 @@ class Job(models.Model):
     title = models.CharField(max_length=50)
     hourly_rate = models.FloatField(default=15.00)
     company = models.CharField(max_length=20)
-    address = models.CharField(models.CharField(max_length=255))
+    address = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
     zip_code = models.CharField(max_length=5)
